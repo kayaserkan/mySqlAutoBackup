81fb1dfbc6ab8386b240b12f85d8f4e7442c3e51
Serkan KAYA <serkankayatr@gmail.com>
2019-11-15 23:41:27 +0300

    Formgorunumu  duzenlendi

--- MySqlYedekAlma/Form1.Designer.cs
+++ MySqlYedekAlma/Form1.Designer.cs
@@ -52,9 +52,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(472, 15);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(470, 153);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(175, 51);
+            this.button1.Size = new System.Drawing.Size(247, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "BİLGİLERİ KAYDET";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,87 +63,92 @@
             // 
             // tbSunucu
             // 
-            this.tbSunucu.Location = new System.Drawing.Point(184, 15);
+            this.tbSunucu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbSunucu.Location = new System.Drawing.Point(262, 15);
             this.tbSunucu.Name = "tbSunucu";
-            this.tbSunucu.Size = new System.Drawing.Size(243, 22);
+            this.tbSunucu.Size = new System.Drawing.Size(455, 27);
             this.tbSunucu.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(18, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 18);
+            this.label1.Size = new System.Drawing.Size(118, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Sunucu Adresi";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(18, 49);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(18, 52);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 18);
+            this.label2.Size = new System.Drawing.Size(113, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "DataBase Adı";
             // 
             // tbDbAdi
             // 
-            this.tbDbAdi.Location = new System.Drawing.Point(184, 49);
+            this.tbDbAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbDbAdi.Location = new System.Drawing.Point(262, 49);
             this.tbDbAdi.Name = "tbDbAdi";
-            this.tbDbAdi.Size = new System.Drawing.Size(243, 22);
+            this.tbDbAdi.Size = new System.Drawing.Size(455, 27);
             this.tbDbAdi.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(18, 117);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(18, 120);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 18);
+            this.label3.Size = new System.Drawing.Size(44, 20);
             this.label3.TabIndex = 8;
             this.label3.Text = "Şifre";
             // 
             // tbSifre
             // 
-            this.tbSifre.Location = new System.Drawing.Point(184, 117);
+            this.tbSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbSifre.Location = new System.Drawing.Point(262, 117);
             this.tbSifre.Name = "tbSifre";
-            this.tbSifre.Size = new System.Drawing.Size(243, 22);
+            this.tbSifre.Size = new System.Drawing.Size(455, 27);
             this.tbSifre.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(18, 83);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(18, 86);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(86, 18);
+            this.label4.Size = new System.Drawing.Size(101, 20);
             this.label4.TabIndex = 6;
             this.label4.Text = "Kullanıcı Adi";
             // 
             // tbKullaniciAdi
             // 
-            this.tbKullaniciAdi.Location = new System.Drawing.Point(184, 83);
+            this.tbKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbKullaniciAdi.Location = new System.Drawing.Point(262, 83);
             this.tbKullaniciAdi.Name = "tbKullaniciAdi";
-            this.tbKullaniciAdi.Size = new System.Drawing.Size(243, 22);
+            this.tbKullaniciAdi.Size = new System.Drawing.Size(455, 27);
             this.tbKullaniciAdi.TabIndex = 5;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label5.Location = new System.Drawing.Point(18, 200);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(118, 18);
+            this.label5.Size = new System.Drawing.Size(133, 20);
             this.label5.TabIndex = 9;
             this.label5.Text = "Yedekleme Saati";
             // 
             // btnBirSeferlikKayit
             // 
-            this.btnBirSeferlikKayit.Location = new System.Drawing.Point(472, 88);
+            this.btnBirSeferlikKayit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBirSeferlikKayit.Location = new System.Drawing.Point(470, 193);
             this.btnBirSeferlikKayit.Name = "btnBirSeferlikKayit";
-            this.btnBirSeferlikKayit.Size = new System.Drawing.Size(175, 51);
+            this.btnBirSeferlikKayit.Size = new System.Drawing.Size(247, 34);
             this.btnBirSeferlikKayit.TabIndex = 12;
             this.btnBirSeferlikKayit.Text = "BİR SEFERLİK BACKUP";
             this.btnBirSeferlikKayit.UseVisualStyleBackColor = true;
@@ -150,9 +156,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(384, 145);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(262, 154);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(43, 34);
+            this.button3.Size = new System.Drawing.Size(32, 34);
             this.button3.TabIndex = 13;
             this.button3.Text = "...";
             this.button3.UseVisualStyleBackColor = true;
@@ -161,25 +168,27 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(18, 154);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(18, 161);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(144, 18);
+            this.label6.Size = new System.Drawing.Size(164, 20);
             this.label6.TabIndex = 14;
             this.label6.Text = "Kayıt Klasörü Seçimi";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(329, 199);
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.Location = new System.Drawing.Point(316, 200);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(112, 21);
+            this.checkBox1.Size = new System.Drawing.Size(130, 24);
             this.checkBox1.TabIndex = 15;
             this.checkBox1.Text = "Düzenli Kayıt";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // numericUpDownSaat
             // 
+            this.numericUpDownSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.numericUpDownSaat.Location = new System.Drawing.Point(184, 198);
             this.numericUpDownSaat.Maximum = new decimal(new int[] {
             23,
@@ -187,11 +196,12 @@
             0,
             0});
             this.numericUpDownSaat.Name = "numericUpDownSaat";
-            this.numericUpDownSaat.Size = new System.Drawing.Size(52, 22);
+            this.numericUpDownSaat.Size = new System.Drawing.Size(52, 27);
             this.numericUpDownSaat.TabIndex = 16;
             // 
             // numericUpDownDakika
             // 
+            this.numericUpDownDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.numericUpDownDakika.Location = new System.Drawing.Point(242, 198);
             this.numericUpDownDakika.Maximum = new decimal(new int[] {
             59,
@@ -199,7 +209,7 @@
             0,
             0});
             this.numericUpDownDakika.Name = "numericUpDownDakika";
-            this.numericUpDownDakika.Size = new System.Drawing.Size(52, 22);
+            this.numericUpDownDakika.Size = new System.Drawing.Size(52, 27);
             this.numericUpDownDakika.TabIndex = 17;
             // 
             // timer1
@@ -212,7 +222,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 231);
+            this.ClientSize = new System.Drawing.Size(729, 231);
             this.Controls.Add(this.numericUpDownDakika);
             this.Controls.Add(this.numericUpDownSaat);
             this.Controls.Add(this.checkBox1);
